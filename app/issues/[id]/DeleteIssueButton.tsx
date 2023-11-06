b0953b05464d84584c39e523a907df795110818f
nkumar007 <naveen.mbuff98@gmail.com>
2023-11-06 18:05:36 +0530

    Add a dialogue box for delete btn

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,7 +1,30 @@
-import {Button} from "@radix-ui/themes";
+"use client";
+import {AlertDialog, Button, Flex} from "@radix-ui/themes";
 
 const DeleteIssueButton = ({IssueId}: {IssueId: number}) => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirmation of Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this Issue?
+        </AlertDialog.Description>
+        <Flex gap="3" mt="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteIssueButton;
