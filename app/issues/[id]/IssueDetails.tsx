8453dbb7c4798f442655457c2194e5bd2654db9e
nkumar007 <naveen.mbuff98@gmail.com>
2023-11-06 12:24:14 +0530

    Refactor:Appy SRP

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import {IssueStatusBadge} from "@/app/components";
+import {Issue} from "@prisma/client";
+import {Heading, Flex, Card} from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+
+const IssueDetails = ({issue}: {issue: Issue}) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex className="space-x-3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <p>{issue.createdAt.toDateString()}</p>
+      </Flex>
+      <Card className="prose" mt="3">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,8 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import {Box, Button, Card, Flex, Grid, Heading} from "@radix-ui/themes";
-import Link from "next/link";
+import {Box, Grid} from "@radix-ui/themes";
 import {notFound} from "next/navigation";
-import {Pencil2Icon} from "@radix-ui/react-icons";
-import ReactMarkdown from "react-markdown";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: {id: string};
@@ -19,20 +17,10 @@ const IssueDetailsPage = async ({params}: Props) => {
   return (
     <Grid columns={{initial: "1", md: "2"}} gap="5">
       <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex className="space-x-3" my="2">
-          <IssueStatusBadge status={issue.status} />
-          <p>{issue.createdAt.toDateString()}</p>
-        </Flex>
-        <Card className="prose" mt="3">
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <EditIssueButton IssueId={issue.id} />
       </Box>
     </Grid>
   );
