ce41f25aad54bee04e2c4375559d78e6fb0e0a5f
nkumar007 <naveen.mbuff98@gmail.com>
2023-11-06 19:31:23 +0530

    remove duplicate skeletons

---
+++ app/issues/edit/[id]/page.tsx
@@ -0,0 +1,24 @@
+import dynamic from "next/dynamic";
+import prisma from "@/prisma/client";
+import {notFound} from "next/navigation";
+import IssueFormSkeleton from "./loading";
+
+interface Props {
+  params: {id: string};
+}
+
+const IssueForm = dynamic(() => import("../../_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
+
+const IssueEditPage = async ({params}: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: {id: parseInt(params.id)},
+  });
+
+  if (!issue) notFound();
+  return <IssueForm issue={issue} />;
+};
+
+export default IssueEditPage;
