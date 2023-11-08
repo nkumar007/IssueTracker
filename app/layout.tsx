3d2e7e030844d705351627917b337165fbb011fe
nkumar007 <naveen.mbuff98@gmail.com>
2023-11-08 21:36:19 +0530

    Add the login and logout links

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import Navbar from "./Navbar";
 import {Container, Theme, ThemePanel} from "@radix-ui/themes";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,12 +21,14 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="purple">
-          <Navbar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor="purple">
+            <Navbar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
