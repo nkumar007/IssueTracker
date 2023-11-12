58a78d7ca0cea74edf5023578f9994f5e4674ae1
nkumar007 <naveen.mbuff98@gmail.com>
2023-11-12 17:06:39 +0530

    Paginating Issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,36 +1,62 @@
-import {Table} from "@radix-ui/themes";
 import prisma from "@/prisma/client";
-import {IssueStatusBadge} from "@/app/components";
-import IssueActions from "./IssueActions";
-import {Link} from "@/app/components";
+import {Table} from "@radix-ui/themes";
+import {IssueStatusBadge, Link} from "@/app/components";
 import NextLink from "next/link";
+import IssueActions from "./IssueActions";
 import {Issue, Status} from "@prisma/client";
 import {ArrowUpIcon} from "@radix-ui/react-icons";
+import Pagination from "@/app/components/Pagination";
 
-const IssuesPage = async ({
-  searchParams,
-}: {
-  searchParams: {status: Status; orderBy: keyof Issue};
-}) => {
-  const columns: {label: string; value: keyof Issue; className?: string}[] = [
+interface Props {
+  searchParams: {
+    status: Status;
+    orderBy: keyof Issue;
+    page: string;
+  };
+}
+
+const IssuesPage = async ({searchParams}: Props) => {
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
     {label: "Issue", value: "title"},
-    {label: "Status", value: "status", className: "hidden md:table-cell"},
-    {label: "CreatedAt", value: "createdAt", className: "hidden md:table-cell"},
+    {
+      label: "Status",
+      value: "status",
+      className: "hidden md:table-cell",
+    },
+    {
+      label: "Created",
+      value: "createdAt",
+      className: "hidden md:table-cell",
+    },
   ];
+
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+  const where = {status};
 
   const orderBy = columns
     .map((column) => column.value)
     .includes(searchParams.orderBy)
     ? {[searchParams.orderBy]: "asc"}
     : undefined;
+
+  const page = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
+
   const issues = await prisma.issue.findMany({
-    where: {status},
+    where,
     orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize,
   });
+
+  const issueCount = await prisma.issue.count({where});
 
   return (
     <div>
@@ -66,7 +92,6 @@ const IssuesPage = async ({
                   <IssueStatusBadge status={issue.status} />
                 </div>
               </Table.Cell>
-
               <Table.Cell className="hidden md:table-cell">
                 <IssueStatusBadge status={issue.status} />
               </Table.Cell>
@@ -77,8 +102,15 @@ const IssuesPage = async ({
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination
+        pageSize={pageSize}
+        currentPage={page}
+        itemCount={issueCount}
+      />
     </div>
   );
 };
+
+export const dynamic = "force-dynamic";
 
 export default IssuesPage;
