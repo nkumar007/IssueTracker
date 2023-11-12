0425d5caa019fd847671b8929d7274882b570b55
nkumar007 <naveen.mbuff98@gmail.com>
2023-11-12 15:32:19 +0530

    Fix filtering bugs

--- app/issues/list/IssueFilterStatus.tsx
+++ app/issues/list/IssueFilterStatus.tsx
@@ -1,35 +1,44 @@
+"use client";
+
 import {Status} from "@prisma/client";
 import {Select} from "@radix-ui/themes";
-import {useRouter} from "next/navigation";
+import {useRouter, useSearchParams} from "next/navigation";
+import React from "react";
 
 const statuses: {label: string; value?: Status}[] = [
   {label: "All"},
   {label: "Open", value: "OPEN"},
-  {label: "Closed", value: "CLOSED"},
   {label: "In Progress", value: "IN_PROGRESS"},
+  {label: "Closed", value: "CLOSED"},
 ];
 
-const IssueFilterStatus = () => {
+const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+
   return (
-    <>
-      <Select.Root
-        onValueChange={(status) => {
-          const query = status ? `?status=${status}` : "";
-          router.push("/issues/list" + query);
-        }}
-      >
-        <Select.Trigger placeholder="Filter by status..." />
-        <Select.Content>
-          {statuses.map((status) => (
-            <Select.Item key={status.value} value={status.value || ""}>
-              {status.label}
-            </Select.Item>
-          ))}
-        </Select.Content>
-      </Select.Root>
-    </>
+    <Select.Root
+      defaultValue={searchParams.get("status") || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+
+        const query = Array.from(params).length ? "?" + params.toString() : "";
+        router.push("/issues/list" + query);
+      }}
+    >
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item key={status.value} value={status.value || ""}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
   );
 };
 
-export default IssueFilterStatus;
+export default IssueStatusFilter;
